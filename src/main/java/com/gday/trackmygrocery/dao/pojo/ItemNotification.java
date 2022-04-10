c0edb55527ff9cb7d6abc81928f16e88b4822c19
axzhx139 <hzha6855@uni.sydney.edu.au>
2022-04-10 15:34:04 +0800

    Notification addDate & RemindDate fix

--- src/main/java/com/gday/trackmygrocery/dao/pojo/ItemNotification.java
+++ src/main/java/com/gday/trackmygrocery/dao/pojo/ItemNotification.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -11,17 +13,17 @@ public class ItemNotification implements Serializable {
     private String name;
     private Integer itemId;
 
-    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
-    private Date addDate;
-    private Integer expireDays;
+    private String addDate;
+    private Integer remindDays;
     private Boolean unread;
 
     public ItemNotification (Item item) {
         this.name = item.getName();
         this.itemId = item.getItemId();
-        this.addDate = item.getAddDate();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        this.addDate = dateFormat.format(item.getAddDate());
         this.unread = item.getUnread();
         Date date = new Date();
-        this.expireDays = (int) ((item.getExpDate().getTime() - date.getTime()) / (1000*3600*24));
+        this.remindDays = (int) ((item.getRemindTime().getTime() - date.getTime()) / (1000*3600*24));
     }
 }
